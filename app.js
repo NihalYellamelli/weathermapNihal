081efe2d896d4b0a932963d57f0f8a00c9b96e09
NihalYellamelli <nihalyellamelli@gmail.com>
2024-09-27 11:31:10 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const  express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express()
+
+const API_KEY = process.env.API_KEY;
+
+app.get("/", (req, res) => {
+    const address = req.query.address;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${API_KEY}`
+
+    axios.get(url).then(response => {
+        const data = response.data
+        const cityName = data.name
+        const temperature = data.main.temp
+        const sunSetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString()
+        const message = `City Name: ${cityName}<br>Temperature: ${temperature}&deg;C<br>Sunset Time: ${sunSetTime}`;
+
+        res.send(`<html><body><div id='container'><h1>${message}</h1></div></body></html>`);
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(500).send("Error occured")
+    })
+})
+
+app.listen(3000, () =>{
+    console.log('server running at port 3000')
+})
